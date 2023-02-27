fea98621c3ea10078f6fb1d6ba82e7fa39c2a482
Elias Martins <eliasgjt@gmail.com>
2023-02-27 14:04:40 -0300

    entity point

---
+++ src/main/java/com/desafio/pontosgps/entities/Point.java
@@ -0,0 +1,85 @@
+package com.desafio.pontosgps.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_point")
+public class Point implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String name;
+	private Integer x;
+	private Integer y;
+	
+	public Point() {
+	}
+
+	public Point(Integer id, String name, Integer x, Integer y) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.x = x;
+		this.y = y;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getX() {
+		return x;
+	}
+
+	public void setX(Integer x) {
+		this.x = x;
+	}
+
+	public Integer getY() {
+		return y;
+	}
+
+	public void setY(Integer y) {
+		this.y = y;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Point other = (Point) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+}
