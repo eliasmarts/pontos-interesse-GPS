e562e8bee901f955dd7f7cc752a1887d368cef5a
Elias Martins <eliasgjt@gmail.com>
2023-02-27 13:51:00 -0300

    setup project

---
+++ src/main/java/com/desafio/pontosgps/PontosgpsApplication.java
@@ -0,0 +1,13 @@
+package com.desafio.pontosgps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PontosgpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PontosgpsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/desafio/pontosgps/PontosgpsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio.pontosgps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PontosgpsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
