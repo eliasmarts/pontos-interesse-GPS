dac6a0b453376923fce671f2b3d10d3f9adc60fe
Elias Martins <eliasgjt@gmail.com>
2023-02-27 15:25:49 -0300

    database instantiation

---
+++ src/main/java/com/desafio/pontosgps/config/Instantiation.java
@@ -0,0 +1,26 @@
+package com.desafio.pontosgps.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.desafio.pontosgps.entities.Point;
+import com.desafio.pontosgps.repositories.PointRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+	@Autowired
+	private PointRepository pointRepository;
+	@Override
+	public void run(String... args) throws Exception {
+		pointRepository.save(new Point(null, "Lanchonete", 27, 12));
+		pointRepository.save(new Point(null, "Posto", 31, 18));
+		pointRepository.save(new Point(null, "Joalheria", 15, 12));
+		pointRepository.save(new Point(null, "Floricultura", 19, 21));
+		pointRepository.save(new Point(null, "Pub", 12, 8));
+		pointRepository.save(new Point(null, "Supermercado", 23, 6));
+		pointRepository.save(new Point(null, "Churrascaria", 28, 2));
+	}
+
+}
